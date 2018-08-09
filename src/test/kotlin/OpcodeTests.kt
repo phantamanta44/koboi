92274694acabe0c52811be0bd0c8a8d8fb9feecb
phantamanta44 <evandalong@gmail.com>
2018-08-08 22:56:17 -0500

    LD unit tests

--- src/test/kotlin/OpcodeTests.kt
+++ src/test/kotlin/OpcodeTests.kt
@@ -1,7 +1,11 @@
 import io.github.phantamanta44.koboi.backtrace.mnemonics
 import io.github.phantamanta44.koboi.cpu.Cpu
 import io.github.phantamanta44.koboi.cpu.FlagRegister
+import io.github.phantamanta44.koboi.cpu.IRegister
 import io.github.phantamanta44.koboi.cpu.Opcodes
+import io.github.phantamanta44.koboi.util.toUnsignedHex
+import io.github.phantamanta44.koboi.util.toUnsignedInt
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestFactory
@@ -233,6 +237,137 @@ class ArithmeticTests : CpuTests() {
                         assertFlag(cpu, FlagRegister::kN, true, "N")
                         assertFlag(cpu, FlagRegister::kH, result and 0xF0 == a and 0xF0, "H")
                         assertFlag(cpu, FlagRegister::kC, o <= a, "C")
+                    }
+                }
+            }
+
+}
+
+class LoadTests : CpuTests() {
+
+    private fun writeRegister8(register: (Cpu) -> IRegister<Byte>): (Cpu, Byte) -> Unit = { cpu, data ->
+        register(cpu).write(data)
+    }
+
+    private fun writePointer8(register: (Cpu) -> IRegister<Short>): (Cpu, Byte) -> Unit = { cpu, data ->
+        cpu.memory.write(register(cpu).read().toUnsignedInt(), data, direct = true)
+    }
+
+    private fun writeLocation8(identifier: String): (Cpu, Byte) -> Unit = when (identifier) {
+        "(BC)" -> writePointer8(Cpu::regBC)
+        "(DE)" -> writePointer8(Cpu::regDE)
+        "(HL)" -> writePointer8(Cpu::regHL)
+        "B" -> writeRegister8(Cpu::regB)
+        "C" -> writeRegister8(Cpu::regC)
+        "D" -> writeRegister8(Cpu::regD)
+        "E" -> writeRegister8(Cpu::regE)
+        "H" -> writeRegister8(Cpu::regH)
+        "L" -> writeRegister8(Cpu::regL)
+        "A" -> writeRegister8(Cpu::regA)
+        "d8" -> { cpu, byte ->
+            cpu.memory.write(0x0007, byte, direct = true)
+            cpu.regPC.write(0x0006)
+        }
+        "(a16)" -> { cpu, byte ->
+            cpu.memory.write(0x0707, byte, direct = true)
+            cpu.memory.write(0x0007, 0x0707, true)
+            cpu.regPC.write(0x0006)
+        }
+        "(a8)" -> { cpu, byte ->
+            cpu.memory.write(0xFF07, byte, direct = true)
+            cpu.memory.write(0x0007, 0x07.toByte(), direct = true)
+            cpu.regPC.write(0x0006)
+        }
+        "(C)" -> { cpu, byte ->
+            cpu.memory.write(0xFF07, byte, direct = true)
+            cpu.regC.write(0x07)
+        }
+        else -> throw IllegalArgumentException(identifier)
+    }
+
+    private fun readRegister8(register: (Cpu) -> IRegister<Byte>): (Cpu) -> Byte = { register(it).read() }
+
+    private fun readPointer8(register: (Cpu) -> IRegister<Short>): (Cpu) -> Byte = {
+        it.memory.read(register(it).read().toUnsignedInt(), true)
+    }
+
+    private fun readLocation8(identifier: String): (Cpu) -> Byte = when (identifier) {
+        "(BC)" -> readPointer8(Cpu::regBC)
+        "(DE)" -> readPointer8(Cpu::regDE)
+        "(HL)" -> readPointer8(Cpu::regHL)
+        "B" -> readRegister8(Cpu::regB)
+        "C" -> readRegister8(Cpu::regC)
+        "D" -> readRegister8(Cpu::regD)
+        "E" -> readRegister8(Cpu::regE)
+        "H" -> readRegister8(Cpu::regH)
+        "L" -> readRegister8(Cpu::regL)
+        "A" -> readRegister8(Cpu::regA)
+        "(a16)" -> { cpu -> cpu.memory.read(0x7070, true) }
+        "(a8)" -> { cpu -> cpu.memory.read(0xFF07, true) }
+        "(C)" -> { cpu -> cpu.memory.read(0xFF07, true) }
+        else -> throw IllegalArgumentException(identifier)
+    }
+
+    private fun setupRead8(identifier: String, cpu: Cpu) {
+        when (identifier) {
+            "(a16)" -> {
+                cpu.memory.write(0x0007, 0x7070, true)
+                cpu.regPC.write(0x0006)
+            }
+            "(a8)" -> {
+                cpu.memory.write(0x0007, 0x07.toByte(), direct = true)
+                cpu.regPC.write(0x0006)
+            }
+            "(C)" -> { cpu.regC.write(0x07) }
+        }
+    }
+
+    @TestFactory
+    fun testSimpleByteLoads(): List<DynamicTest> = listOf(Triple(0x02, "(BC)", "A"), Triple(0x06, "B", "d8"),
+            Triple(0x0A, "A", "(BC)"), Triple(0x0E, "C", "d8"), Triple(0x12, "(DE)", "A"), Triple(0x16, "D", "d8"),
+            Triple(0x1A, "A", "(DE)"), Triple(0x1E, "E", "d8"), Triple(0x26, "H", "d8"), Triple(0x2E, "L", "d8"),
+            Triple(0x36, "(HL)", "d8"), Triple(0x3E, "A", "d8"), Triple(0x40, "B", "B"), Triple(0x41, "B", "C"),
+            Triple(0x42, "B", "D"), Triple(0x43, "B", "E"), Triple(0x44, "B", "H"), Triple(0x45, "B", "L"),
+            Triple(0x46, "B", "(HL)"), Triple(0x47, "B", "A"), Triple(0x48, "C", "B"), Triple(0x49, "C", "C"),
+            Triple(0x4A, "C", "D"), Triple(0x4B, "C", "E"), Triple(0x4C, "C", "H"), Triple(0x4D, "C", "L"),
+            Triple(0x4E, "C", "(HL)"), Triple(0x4F, "C", "A"), Triple(0x50, "D", "B"), Triple(0x51, "D", "C"),
+            Triple(0x52, "D", "D"), Triple(0x53, "D", "E"), Triple(0x54, "D", "H"), Triple(0x55, "D", "L"),
+            Triple(0x56, "D", "(HL)"), Triple(0x57, "D", "A"), Triple(0x58, "E", "B"), Triple(0x59, "E", "C"),
+            Triple(0x5A, "E", "D"), Triple(0x5B, "E", "E"), Triple(0x5C, "E", "H"), Triple(0x5D, "E", "L"),
+            Triple(0x5E, "E", "(HL)"), Triple(0x5F, "E", "A"), Triple(0x60, "H", "B"), Triple(0x61, "H", "C"),
+            Triple(0x62, "H", "D"), Triple(0x63, "H", "E"), Triple(0x64, "H", "H"), Triple(0x65, "H", "L"),
+            Triple(0x66, "H", "(HL)"), Triple(0x67, "H", "A"), Triple(0x68, "L", "B"), Triple(0x69, "L", "C"),
+            Triple(0x6A, "L", "D"), Triple(0x6B, "L", "E"), Triple(0x6C, "L", "H"), Triple(0x6D, "L", "L"),
+            Triple(0x6E, "L", "(HL)"), Triple(0x6F, "L", "A"), Triple(0x70, "(HL)", "B"), Triple(0x71, "(HL)", "C"),
+            Triple(0x72, "(HL)", "D"), Triple(0x73, "(HL)", "E"), Triple(0x74, "(HL)", "H"), Triple(0x75, "(HL)", "L"),
+            Triple(0x77, "(HL)", "A"), Triple(0x78, "A", "B"), Triple(0x79, "A", "C"), Triple(0x7A, "A", "D"),
+            Triple(0x7B, "A", "E"), Triple(0x7C, "A", "H"), Triple(0x7D, "A", "L"), Triple(0x7E, "A", "(HL)"),
+            Triple(0x7F, "A", "A"), Triple(0xE0, "(a8)", "A"), Triple(0xE2, "(C)", "A"), Triple(0xEA, "(a16)", "A"),
+            Triple(0xF0, "A", "(a8)"), Triple(0xF2, "A", "(C)"), Triple(0xFA, "A", "(a16)"))
+            .map {
+                DynamicTest.dynamicTest(mnemonics[it.first]) {
+                    val rand = Random()
+                    for (i in 0..511) {
+                        val target = rand.nextInt(256).toByte()
+                        writeLocation8(it.third)(cpu, target)
+                        setupRead8(it.second, cpu)
+                        Opcodes[it.first.toByte()](cpu)
+                        Assertions.assertEquals(target, readLocation8(it.second)(cpu), "${it.second}=${target.toUnsignedHex()}")
+                    }
+                }
+            }
+
+    @TestFactory
+    fun testShortRegisterLoads(): List<DynamicTest> = listOf(0x01 to Cpu::regBC, 0x11 to Cpu::regDE, 0x21 to Cpu::regHL, 0x31 to Cpu::regSP)
+            .map {
+                DynamicTest.dynamicTest(mnemonics[it.first]) {
+                    val rand = Random()
+                    for (i in 0..511) {
+                        val target = rand.nextInt(65536).toShort()
+                        cpu.memory.write(0x7071, target, direct = true)
+                        cpu.regPC.write(0x7070)
+                        Opcodes[it.first.toByte()](cpu)
+                        assertRegister(it.second.get(cpu), target, "${it.second.name}=${target.toUnsignedHex()}")
                     }
                 }
             }
